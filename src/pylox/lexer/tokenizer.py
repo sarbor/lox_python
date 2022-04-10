ce2d569eb003c1e3bd082040b5d1dcc18f8669a5
sarbor <sarboroy99@gmail.com>
2022-04-09 23:37:00 -0700

    Added reserve keywords and identifiers to tokenizer

--- src/pylox/lexer/tokenizer.py
+++ src/pylox/lexer/tokenizer.py
@@ -11,6 +11,7 @@ class Tokenizer:
       self.start = 0
       self.line_num = 1
       self.error = None
+      self.reserved_keywords = self._initialize_reserved_keywords()
 
    def _current_text(self) -> str:
       return self.source[self.start:self.current+1]
@@ -100,6 +101,8 @@ class Tokenizer:
          return Token_Type.SLASH, err
       elif current_text.isdigit():
          return self._get_number_token()
+      elif current_text.isalpha():
+         return self._get_identifier_token()
       elif current_text == '\"':
          return self._get_string_token()
       elif current_text == '*':
@@ -133,7 +136,7 @@ class Tokenizer:
          return None, err
       elif current_text == ' ':
          return None, err
-      elif current_text == _:
+      else:
          return None, Lox_Err(msg=f'{self._current_text()} is not a valid token', line_num=self.line_num)
 
    def _get_number_token(self):
@@ -147,6 +150,19 @@ class Tokenizer:
             self._increment_cursor()
 
       return Token_Type.NUMBER, err
+
+   def _get_identifier_token(self):
+      err = None
+
+      while (next_char := self._peek()) and next_char.isalpha():
+         if next_char == '\n':
+            self.line_num += 1
+         self._increment_cursor()
+
+      if self._current_text() in self.reserved_keywords:
+         return self.reserved_keywords[self._current_text()], err
+
+      return Token_Type.IDENTIFIER, err
 
    def _get_string_token(self):
       err = None
@@ -165,6 +181,27 @@ class Tokenizer:
 
       return Token_Type.STRING, err
 
+
+   def _initialize_reserved_keywords(self):
+      reserved_keyword_to_token_type = dict()
+      reserved_keyword_to_token_type['and'] = Token_Type.AND
+      reserved_keyword_to_token_type['class'] = Token_Type.CLASS
+      reserved_keyword_to_token_type['else'] = Token_Type.ELSE
+      reserved_keyword_to_token_type['false'] = Token_Type.FALSE
+      reserved_keyword_to_token_type['for'] = Token_Type.FOR
+      reserved_keyword_to_token_type['fun'] = Token_Type.FUN
+      reserved_keyword_to_token_type['if'] = Token_Type.IF
+      reserved_keyword_to_token_type['nil'] = Token_Type.NIL
+      reserved_keyword_to_token_type['or'] = Token_Type.OR
+      reserved_keyword_to_token_type['print'] = Token_Type.PRINT
+      reserved_keyword_to_token_type['return'] = Token_Type.RETURN
+      reserved_keyword_to_token_type['super'] = Token_Type.SUPER
+      reserved_keyword_to_token_type['this'] = Token_Type.THIS
+      reserved_keyword_to_token_type['true'] = Token_Type.TRUE
+      reserved_keyword_to_token_type['var'] = Token_Type.VAR
+      reserved_keyword_to_token_type['while'] = Token_Type.WHILE
+
+      return reserved_keyword_to_token_type
 
 
 
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -117,5 +117,124 @@ class TestTokenizer(unittest.TestCase):
       self.assertIsNone(err)
       self.assertEqual(returned_tokens, expected_tokens)
 
+   def test_and_keyword(self):
+      lox_program = 'and'
+      expected_tokens = [Token(Token_Type.AND, 'and', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_class_keyword(self):
+      lox_program = 'class'
+      expected_tokens = [Token(Token_Type.CLASS, 'class', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_else_keyword(self):
+      lox_program = 'else'
+      expected_tokens = [Token(Token_Type.ELSE, 'else', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_false_keyword(self):
+      lox_program = 'false'
+      expected_tokens = [Token(Token_Type.FALSE, 'false', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_for_keyword(self):
+      lox_program = 'for'
+      expected_tokens = [Token(Token_Type.FOR, 'for', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_fun_keyword(self):
+      lox_program = 'fun'
+      expected_tokens = [Token(Token_Type.FUN, 'fun', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_if_keyword(self):
+      lox_program = 'if'
+      expected_tokens = [Token(Token_Type.IF, 'if', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_nil_keyword(self):
+      lox_program = 'nil'
+      expected_tokens = [Token(Token_Type.NIL, 'nil', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_or_keyword(self):
+      lox_program = 'or'
+      expected_tokens = [Token(Token_Type.OR, 'or', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_print_keyword(self):
+      lox_program = 'print'
+      expected_tokens = [Token(Token_Type.PRINT, 'print', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_return_keyword(self):
+      lox_program = 'return'
+      expected_tokens = [Token(Token_Type.RETURN, 'return', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_super_keyword(self):
+      lox_program = 'super'
+      expected_tokens = [Token(Token_Type.SUPER, 'super', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_this_keyword(self):
+      lox_program = 'this'
+      expected_tokens = [Token(Token_Type.THIS, 'this', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_true_keyword(self):
+      lox_program = 'true'
+      expected_tokens = [Token(Token_Type.TRUE, 'true', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_var_keyword(self):
+      lox_program = 'var'
+      expected_tokens = [Token(Token_Type.VAR, 'var', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_while_keyword(self):
+      lox_program = 'while'
+      expected_tokens = [Token(Token_Type.WHILE, 'while', 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
+   def test_identifiers(self):
+      lox_program = 'identifier'
+      expected_tokens = [Token(Token_Type.IDENTIFIER, lox_program, 1, None), self._create_EOF_token()]
+      returned_tokens, err = self.tokenizer.tokenize(lox_program)
+      self.assertIsNone(err)
+      self.assertEqual(returned_tokens, expected_tokens)
+
 if __name__ == '__main__':
    unittest.main()
